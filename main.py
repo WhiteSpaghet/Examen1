0f171b99e7efb7b064dd46ae67a1f2ce8b9682b5
WhiteSpaghet <superivasca@gmail.com>
2025-03-28 10:05:53 +0100

    Main1

---
+++ main.py
@@ -0,0 +1,15 @@
+from punto import Punto
+from rectangulo import Rectangulo
+
+def main():
+    # Crear puntos A, B, C y D
+    A = Punto(2, 3)
+    B = Punto(5, 5)
+    C = Punto(-3, -1)
+    D = Punto(0, 0)
+
+    # Imprimir puntos
+    print(f"Punto A: {A}")
+    print(f"Punto B: {B}")
+    print(f"Punto C: {C}")
+    print(f"Punto D: {D}")
