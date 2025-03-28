a90204f2c368d37af25d405584f53abdd9f9ad71
WhiteSpaghet <superivasca@gmail.com>
2025-03-28 10:01:57 +0100

    Rect345

--- rectangulo.py
+++ rectangulo.py
@@ -4,3 +4,12 @@ class Rectangulo:
     def __init__(self, punto_inicial=(0, 0), punto_final=(0, 0)):
         self.punto_inicial = punto_inicial
         self.punto_final = punto_final
+
+    def base(self):
+        return abs(self.punto_final[0] - self.punto_inicial[0])
+
+    def altura(self):
+        return abs(self.punto_final[1] - self.punto_inicial[1])
+
+    def area(self):
+        return self.base() * self.altura()
